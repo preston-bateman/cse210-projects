5e4fdd7eceb6becde1b80e2e5d4903f1230a3594
Preston Bateman <prestonbbateman@gmail.com>
2023-01-06 16:13:29 -0700

    week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the number grade of your assignment?");
+        string numGradeString = Console.ReadLine();
+        int numGrade = int.Parse(numGradeString);
+        string letter = null;
+        string plusOrMinus = null;
+
+        if (numGrade < 60) {
+            letter = "F";
+        }
+        else if (numGrade < 70) {
+            letter = "D";
+        }
+        else if (numGrade < 80) {
+            letter = "C";
+        }
+        else if (numGrade < 90) {
+            letter = "B";
+        }
+        else if (numGrade <= 100) {
+            letter = "A";
+        }
+
+        if (numGrade % 10 >= 7 ) {
+            plusOrMinus = "+";
+        }
+        else if (numGrade % 10 < 3) {
+            plusOrMinus = "-";
+        }
+        else {
+            plusOrMinus = null;
+        }
+
+        if (plusOrMinus == "-" || plusOrMinus == "+") {
+            letter += plusOrMinus;
+        }
+        else {
+            letter = letter;
+        }
+
+        if (letter == "F") {
+            Console.WriteLine($"Sorry you failed with an {letter}");
+        }
+        else {
+            Console.WriteLine($"You made an {letter}");
+        }
+
+
     }
 }
