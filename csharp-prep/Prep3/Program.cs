903f237b939c800631b17591b2ad4c30d5850bd2
Preston Bateman <prestonbbateman@gmail.com>
2023-01-18 21:37:33 -0700

    prep update compare teacher

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+
+        int guess = -10;
+        int count = 0;
+
+        while(guess != mNumber) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (mNumber < guess) {
+                Console.WriteLine("lower");
+                count++;
+            }
+            else if (mNumber > guess) {
+                Console.WriteLine("higher");
+                count++;
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {count} guesses to win!");
+            }
+        }
     }
 }
