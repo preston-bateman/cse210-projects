0674a6bbec273f4e3ff8149eab1c6f978ef85bdc
Preston Bateman <prestonbbateman@gmail.com>
2023-01-18 21:53:29 -0700

    updated prep compare with teacher

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> num = new List<int>();
+        int number = 0;
+
+        do {
+            Console.Write("Enter a number (0 to quit): ");
+            string newNum = Console.ReadLine();
+            number = int.Parse(newNum);
+
+            if (number != 0) {
+                num.Add(number);
+            }
+
+        }while (number != 0);
+
+        int sum = 0;
+        foreach (int i in num) {
+            sum += i;
+        }
+        Console.WriteLine($"The Sum Is: {sum}");
+
+        float avg = ((float)sum) / num.Count;
+        Console.WriteLine($"The Average Is: {avg}");
+
+        int maximum = num[0];
+
+        foreach (int i in num) {
+            if (i > maximum) {
+                maximum = i;
+            }
+        }
+        Console.WriteLine($"The Max Is: {maximum}");
     }
 }
