310c10c7c9e4a7de877cdb74f500aed31ed090d5
Preston Bateman <prestonbbateman@gmail.com>
2023-01-18 22:45:50 -0700

    abstraction work final submit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Fourth Officer";
+        job1._company = "White Star Line, SS Celtic";
+        job1._startYear = 1880;
+        job1._endYear = 1887;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Ships Captain";
+        job2._company = "White Star Line, RMS Titanic";
+        job2._startYear = 1912;
+        job2._endYear = 1912;
+
+        Resume resume = new Resume();
+        resume._name = "Edward Smith";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
