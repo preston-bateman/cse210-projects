d0520b187eb768670039f85f9dcee40a062fd5a3
Preston Bateman <prestonbbateman@gmail.com>
2023-01-25 21:05:25 -0700

    sdd

---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Entry {
+    public string entryDate;
+    public string prompt;
+    public string response;
+
+    public Entry(string selectedPrompt){
+        prompt = selectedPrompt;
+        entryDate = GenerateDate();
+        response = GetResponse();
+    }
+
+    public string GetResponse(){
+        Console.WriteLine($"{prompt}:");
+        string entry = Console.ReadLine();
+        return entry;
+    }
+    public string GenerateDate(){
+        var todayDate = DateTime.Today;
+        string todayString = todayDate.ToString();
+        return todayString;
+    }
+
+}
---
+++ prove/Develop02/journal.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal{
+    public List<Entry> entries = new List<Entry> ();
+
+
+}
