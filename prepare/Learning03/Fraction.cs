cb8fdc49b34e94b51a609ef09519c714e29ba80a
Preston Bateman <prestonbbateman@gmail.com>
2023-01-31 15:28:07 -0700

    fraction commit

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Fraction{
+    private int _top;
+    private int _bottom;
+
+    public Fraction (){
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber){
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop(){
+        return _top;
+    }
+    public void SetTop(int top){
+        _top = top;
+    }
+    public int GetBottom(){
+        return _bottom;
+    }
+    public void SetBottom(int bottom) {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString(){
+        return _top.ToString() + "/" + _bottom.ToString();
+    }
+    public double GetDecimalValue(){
+        double newtop =  Convert.ToDouble(_top);
+        double newbottom = Convert.ToDouble(_bottom);
+        return newtop / newbottom;
+    }
+
+}
